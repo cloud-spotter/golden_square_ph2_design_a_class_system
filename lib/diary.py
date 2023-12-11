ec92c427b28b9aa34638747e7414181fe16e02df
cloud-spotter <cloudspotter0@gmail.com>
2023-12-11 15:19:42 +0000

    3 Diary integration tests passing; reading_time and find_best_entry_for_reading_time methods implemented on Diary

--- lib/diary.py
+++ lib/diary.py
@@ -19,3 +19,20 @@ class Diary:
         for entry in self.entries:
             word_count += entry.count_words()
         return word_count
+    
+    def reading_time(self, wpm: int) -> int:
+        '''Returns an integer representing an estimate of the reading
+        time in minutes, if the user were to read all diary entries'''
+        return self.count_words() / wpm
+    
+    def find_best_entry_for_reading_time(self, wpm: int, minutes: int):
+        '''Returns an instance of DiaryEntry representing the entry that
+        is closest to, but not over, the length that the user could read
+        in the minutes they have available given their reading speed.'''
+        word_limit = wpm * minutes
+        sorted_entries = sorted(self.entries, key=lambda entry: entry.count_words(), reverse=True)
+        # Include reverse=True in sorted method to catch any entry equal to word_limit first
+        for entry in sorted_entries:
+            if entry.count_words() <= word_limit:
+                return entry
+        
